741803cbb4db3a7aa9a9cc5e1abeec6997d46c9b
Vaibhav Acharya <vaibhavacharya111@gmail.com>
2020-11-25 16:48:27 +0530

    pritty

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 import { CssBaseline, GeistProvider } from "@geist-ui/react";
 import { UserProvider } from "../components/firebase/UserContext";
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
-import { Button, Card, Description, Divider, Link, Spacer, Text } from "@geist-ui/react";
+import {
+  Button,
+  Card,
+  Description,
+  Divider,
+  Link,
+  Spacer,
+  Text,
+} from "@geist-ui/react";
 import useUser from "../components/firebase/useUser";
 import Layout from "../components/Layout";
 
@@ -11,7 +19,9 @@ export default function Home() {
 
   return (
     <Layout>
-      <Text h4 type="secondary">Smarter. Faster. Better.</Text>
+      <Text h4 type="secondary">
+        Smarter. Faster. Better.
+      </Text>
       <Text h2>A quiz management app which takes away the burden.</Text>
       <Spacer y />
       <Link href="/authenticate">
@@ -19,13 +29,24 @@ export default function Home() {
       </Link>
       <Spacer y />
       <Card>
-        <Text h4 type="warning">Features</Text>
+        <Text h4 type="warning">
+          Features
+        </Text>
         <Divider />
-        <Description title="Automatic Results" content="It automatically generates results an eliminates the chances of mistakes and at the same time saves your valuable time." />
+        <Description
+          title="Automatic Results"
+          content="It automatically generates results an eliminates the chances of mistakes and at the same time saves your valuable time."
+        />
         <Spacer />
-        <Description title="Easy Management" content="It makes it dead simple to manage even hundreds of quizzes at the same time on the same spot." />
+        <Description
+          title="Easy Management"
+          content="It makes it dead simple to manage even hundreds of quizzes at the same time on the same spot."
+        />
         <Spacer />
-        <Description title="Fast Creation" content="It becomes very easy to create even big quizzes thanks to our beautiful UI." />
+        <Description
+          title="Fast Creation"
+          content="It becomes very easy to create even big quizzes thanks to our beautiful UI."
+        />
       </Card>
     </Layout>
   );
